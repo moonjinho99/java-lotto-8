29b737ed648f5b7c03e86883fd9fcc1d64261992
moonjinho99 <anszl789@naver.com>
2025-10-29 15:29:47 +0900

    feat(inputview) : 입력 기능 구현, InputView 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,9 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        inputView.retryUntilVaild(inputView::getLottoAmount);
+        inputView.retryUntilVaild(inputView::getWinningNumbers);
+        inputView.retryUntilVaild(inputView::getBonusNumber);
     }
 }
---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,66 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class InputView {
+
+    public int getLottoAmount()
+    {
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+
+        if(amount % 1000 != 0)
+        {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000단위로 입력해야 합니다.");
+        }
+
+        return amount;
+    }
+
+    public List<Integer> getWinningNumbers()
+    {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] inputs = Console.readLine().split(",");
+
+        for(String input : inputs) {
+            int num = Integer.parseInt(input);
+
+            if(num < 1 || num > 45)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
+            winningNumbers.add(num);
+
+        }
+
+        return winningNumbers;
+    }
+
+    public int getBonusNumber()
+    {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        if(bonusNumber < 1 || bonusNumber > 45)
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
+        return bonusNumber;
+    }
+
+    public <T> T retryUntilVaild(Supplier<T> action)
+    {
+        while(true) {
+            try{
+                return action.get();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            } catch (IllegalStateException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
